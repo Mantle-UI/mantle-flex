389ef3f65c639188284a7fb254e940c82e3f1438
Sven Jansen <sja@cslab.de>
2026-07-27 17:41:49 +0200

    ci: deploy Flex docs to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+const basePath = isGitHubPages ? '/mantle-flex' : '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    trailingSlash: true,
+    assetPrefix: isGitHubPages ? `${basePath}/` : '',
+    basePath,
+    env: {
+        NEXT_PUBLIC_BASE_PATH: basePath
+    }
 };
 
 module.exports = nextConfig;
